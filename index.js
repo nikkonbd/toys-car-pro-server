cf5b8a47997d41a710e282c2d79a0cff7123163b
nikkonbd <nikkon1998@gmail.com>
2023-05-21 22:42:48 +0600

    all done server

--- index.js
+++ index.js
@@ -77,7 +77,6 @@ async function run() {
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        // await client.close();
     }
 }
 run().catch(console.dir);
